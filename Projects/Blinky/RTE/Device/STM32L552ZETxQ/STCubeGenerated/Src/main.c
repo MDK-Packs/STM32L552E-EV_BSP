58ebc1347b5b344dd56748e6937cbc8f77f44775
Ziga Mahne <ziga.mahne@arm.com>
2021-08-30 09:26:45 +0200

    Projects examples update
    - override default HAL_InitTick function
    Replace documentation files with links

--- Projects/Blinky/RTE/Device/STM32L552ZETxQ/STCubeGenerated/Src/main.c
+++ Projects/Blinky/RTE/Device/STM32L552ZETxQ/STCubeGenerated/Src/main.c
@@ -62,7 +62,6 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 /**
   * Override default HAL_GetTick function
   */
@@ -81,6 +80,16 @@ uint32_t HAL_GetTick (void) {
     __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP();
   }
   return ++ticks;
+}
+
+/**
+  * Override default HAL_InitTick function
+  */
+HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority) {
+  
+  UNUSED(TickPriority);
+
+  return HAL_OK;
 }
 /* USER CODE END 0 */
 
