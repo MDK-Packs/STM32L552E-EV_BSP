1f2e9804e70d72fd8963a4469c1a5f300968974a
DomenGosek <domen.gosek@arm.com>
2023-10-04 13:05:54 +0200

    Update STM32L552E-EV_BSP
    - Update VIO to API 1.0.0
    - Update VIO version in .pdsc
    - Add CMSIS-View EVR component to projects
    - Update projects to CMSIS 6
    - Update rtx_config

--- Drivers/Platform/vio_STM32L552E-EV.c
+++ Drivers/Platform/vio_STM32L552E-EV.c
@@ -1,11 +1,11 @@
 /******************************************************************************
  * @file     vio_STM32L552E-EV.c
  * @brief    Virtual I/O implementation for board STM32L552E-EV
- * @version  V1.2.0
- * @date     9. November 2022
+ * @version  V2.0.0
+ * @date     3. October 2023
  ******************************************************************************/
 /*
- * Copyright (c) 2020-2022 Arm Limited (or its affiliates).
+ * Copyright (c) 2020-2023 Arm Limited (or its affiliates).
  * All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -41,12 +41,17 @@ vioLED2           | vioSignalOut.2 | MFX_IO11: LD6 YELLOW                       
 vioLED3           | vioSignalOut.3 | MFX_IO13: LD7 ORANGE                           |
 */
 
-#include <stdio.h>
+/* History:
+ *  Version 2.0.0
+ *    Updated to API 1.0.0
+ *  Version 1.0.0
+ *    Initial release
+ */
+
 #include <string.h>
-#include <stdarg.h>
 #include "cmsis_vio.h"
 
-#include "RTE_Components.h"
+#include "RTE_Components.h"                 // Component selection
 #include CMSIS_device_header
 
 #if !defined CMSIS_VOUT || !defined CMSIS_VIN
@@ -54,102 +59,16 @@ vioLED3           | vioSignalOut.3 | MFX_IO13: LD7 ORANGE                       
 #endif
 
 // VIO input, output definitions
-#define VIO_PRINT_MAX_SIZE      64U     // maximum size of print memory
-#define VIO_PRINTMEM_NUM         4U     // number of print memories
-#define VIO_VALUE_NUM            3U     // number of values
-#define VIO_VALUEXYZ_NUM         3U     // number of XYZ values
-#define VIO_IPV4_ADDRESS_NUM     2U     // number of IPv4 addresses
-#define VIO_IPV6_ADDRESS_NUM     2U     // number of IPv6 addresses
+#define VIO_VALUE_NUM           3U          // Number of values
 
 // VIO input, output variables
-__USED uint32_t      vioSignalIn;                                       // Memory for incoming signal
-__USED uint32_t      vioSignalOut;                                      // Memory for outgoing signal
-__USED char          vioPrintMem[VIO_PRINTMEM_NUM][VIO_PRINT_MAX_SIZE]; // Memory for the last value for each level
-__USED int32_t       vioValue   [VIO_VALUE_NUM];                        // Memory for value used in vioGetValue/vioSetValue
-__USED vioValueXYZ_t vioValueXYZ[VIO_VALUEXYZ_NUM];                     // Memory for XYZ value for 3-D vector
-__USED vioAddrIPv4_t vioAddrIPv4[VIO_IPV4_ADDRESS_NUM];                 // Memory for IPv4 address value used in vioSetIPv4/vioGetIPv4
-__USED vioAddrIPv6_t vioAddrIPv6[VIO_IPV6_ADDRESS_NUM];                 // Memory for IPv6 address value used in vioSetIPv6/vioGetIPv6
+__USED uint32_t vioSignalIn;                // Memory for incoming signal
+__USED uint32_t vioSignalOut;               // Memory for outgoing signal
+__USED int32_t  vioValue[VIO_VALUE_NUM];    // Memory for value used in vioGetValue/vioSetValue
 
 #if !defined CMSIS_VOUT
-// Global types, variables, functions
-static char         ip_ascii[40];       // string buffer for IP address conversion
+// Add global user types, variables, functions here:
 
-/**
-  convert IP4 address to ASCII
-
-  \param[in]   ip4_addr   pointer to IP4 address.
-  \param[out]  buf        pointer to ascii buffer.
-  \param[in]   buf_len    length of a buffer (16 bytes)
-*/
-static void ip4_2a (const uint8_t *ip4_addr, char *buf, uint32_t buf_len) {
-  if (buf_len < 16U) {
-    return;
-  }
-  sprintf(buf, "%d.%d.%d.%d", ip4_addr[0], ip4_addr[1], ip4_addr[2], ip4_addr[3]);
-}
-
-/**
-  convert IP6 address to ASCII
-
-  \param[in]   ip6_addr   pointer to IP6 address.
-  \param[out]  buf        pointer to ascii buffer.
-  \param[in]   buf_len    length of a buffer (40 bytes)
-*/
-static void ip6_2a (const uint8_t *ip6_addr, char *buf, uint32_t buf_len) {
-  uint16_t v16[8];
-  int32_t i, j, nmax, idx;
-
-  if (buf_len < 40U) {
-    return;
-  }
-
-  /* Read IPv6 address in hextets */
-  for (i = 0; i < 16; i += 2) {
-    v16[i >> 1] = (uint16_t)(ip6_addr[i] << 8) | ip6_addr[i+1];
-  }
-
-  /* Find the largest block of consecutive zero hextets */
-  idx = 8;
-  for (i = nmax = 0; i < 8-1; i++) {
-    if (v16[i] != 0U) {
-      continue;
-    }
-    for (j = i; j < 8-1; j++) {
-      if (v16[j+1] != 0U) {
-        break;
-      }
-    }
-    if (i == j) {
-      continue;
-    }
-    if ((j - i) >= nmax) {
-      /* Remember position and count */
-      nmax = j - i + 1;
-      idx  = i;
-    }
-    /* Skip already processed zero hextets */
-    i = j;
-  }
-  for (i = j = 0; i < idx;  ) {
-    j += sprintf(&buf[j], "%x", v16[i]);
-    if (++i == idx) {
-      break;
-    }
-    buf[j++] = ':';
-  }
-  if (i < 8) {
-    /* Right-end not yet complete */
-    buf[j++] = ':';
-    for (i += nmax; i < 8; i++) {
-      j += sprintf(&buf[j], ":%x", v16[i]);
-    }
-    if (buf[j-1] == ':') {
-      buf[j++] = ':';
-    }
-  }
-  /* Make string null-terminated */
-  buf[j] = 0;
-}
 #endif
 
 #if !defined CMSIS_VIN
@@ -171,11 +90,7 @@ void vioInit (void) {
   vioSignalIn  = 0U;
   vioSignalOut = 0U;
 
-  memset(vioPrintMem, 0, sizeof(vioPrintMem));
-  memset(vioValue,    0, sizeof(vioValue));
-  memset(vioValueXYZ, 0, sizeof(vioValueXYZ));
-  memset(vioAddrIPv4, 0, sizeof(vioAddrIPv4));
-  memset(vioAddrIPv6, 0, sizeof(vioAddrIPv6));
+  memset(vioValue, 0, sizeof(vioValue));
 
 #if !defined CMSIS_VOUT
   // Initialize LEDs pins
@@ -192,37 +107,6 @@ void vioInit (void) {
 
   BSP_JOY_Init(JOY1, JOY_MODE_GPIO, JOY_ALL);
 #endif
-}
-
-// Print formatted string to test terminal.
-int32_t vioPrint (uint32_t level, const char *format, ...) {
-  va_list args;
-  int32_t ret;
-#if !defined CMSIS_VOUT
-// Add user variables here:
-
-#endif
-
-  if (level > vioLevelError) {
-    return (-1);
-  }
-
-  if (level > VIO_PRINTMEM_NUM) {
-    return (-1);
-  }
-
-  va_start(args, format);
-
-  ret = vsnprintf((char *)vioPrintMem[level], sizeof(vioPrintMem[level]), format, args);
-
-  va_end(args);
-
-#if !defined CMSIS_VOUT
-// Add user code here:
-
-#endif
-
-  return (ret);
 }
 
 // Set signal output.
@@ -335,9 +219,9 @@ uint32_t vioGetSignal (uint32_t mask) {
 #endif
 #endif
 
-  signal = vioSignalIn;
+  signal = vioSignalIn & mask;
 
-  return (signal & mask);
+  return signal;
 }
 
 // Set value output.
@@ -382,135 +266,4 @@ int32_t vioGetValue (uint32_t id) {
   value = vioValue[index];
 
   return value;
-}
-
-// Set XYZ value output.
-void vioSetXYZ (uint32_t id, vioValueXYZ_t valueXYZ) {
-  uint32_t index = id;
-#if !defined CMSIS_VOUT
-// Add user variables here:
-
-#endif
-
-  if (index >= VIO_VALUEXYZ_NUM) {
-    return;                             /* return in case of out-of-range index */
-  }
-
-  vioValueXYZ[index] = valueXYZ;
-
-#if !defined CMSIS_VOUT
-// Add user code here:
-
-#endif
-}
-
-// Get XYZ value input.
-vioValueXYZ_t vioGetXYZ (uint32_t id) {
-  uint32_t index = id;
-  vioValueXYZ_t valueXYZ = {0, 0, 0};
-#if !defined CMSIS_VIN
-// Add user variables here:
-#endif
-
-  if (index >= VIO_VALUEXYZ_NUM) {
-    return valueXYZ;                    /* return default in case of out-of-range index */
-  }
-
-#if !defined CMSIS_VIN
-// Add user code here:
-
-#endif
-
-  valueXYZ = vioValueXYZ[index];
-
-  return valueXYZ;
-}
-
-// Set IPv4 address output.
-void vioSetIPv4 (uint32_t id, vioAddrIPv4_t addrIPv4) {
-  uint32_t index = id;
-#if !defined CMSIS_VOUT
-// Add user variables here:
-
-#endif
-
-  if (index >= VIO_IPV4_ADDRESS_NUM) {
-    return;                             /* return in case of out-of-range index */
-  }
-
-  vioAddrIPv4[index] = addrIPv4;
-
-#if !defined CMSIS_VOUT
-  // Convert IP4 address to ASCII
-  ip4_2a((uint8_t *)&vioAddrIPv4[index], ip_ascii, sizeof(ip_ascii));
-#endif
-}
-
-// Get IPv4 address input.
-vioAddrIPv4_t vioGetIPv4 (uint32_t id) {
-  uint32_t index = id;
-  vioAddrIPv4_t addrIPv4 = { {0U, 0U, 0U, 0U} };
-#if !defined CMSIS_VIN
-// Add user variables here:
-
-#endif
-
-  if (index >= VIO_IPV4_ADDRESS_NUM) {
-    return addrIPv4;                    /* return default in case of out-of-range index */
-  }
-
-#if !defined CMSIS_VIN
-// Add user code here:
-
-//   vioAddrIPv4[index] = ...;
-#endif
-
-  addrIPv4 = vioAddrIPv4[index];
-
-  return addrIPv4;
-}
-
-// Set IPv6 address output.
-void vioSetIPv6 (uint32_t id, vioAddrIPv6_t addrIPv6) {
-  uint32_t index = id;
-#if !defined CMSIS_VOUT
-// Add user variables here:
-
-#endif
-
-  if (index >= VIO_IPV6_ADDRESS_NUM) {
-    return;                             /* return in case of out-of-range index */
-  }
-
-  vioAddrIPv6[index] = addrIPv6;
-
-#if !defined CMSIS_VOUT
-  // Convert IP6 address to ASCII
-  ip6_2a((uint8_t *)&vioAddrIPv6[index], ip_ascii, sizeof(ip_ascii));
-#endif
-}
-
-// Get IPv6 address input.
-vioAddrIPv6_t vioGetIPv6 (uint32_t id) {
-  uint32_t index = id;
-  vioAddrIPv6_t addrIPv6 = { {0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U,
-                              0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U} };
-#if !defined CMSIS_VIN
-// Add user variables here:
-
-#endif
-
-  if (index >= VIO_IPV6_ADDRESS_NUM) {
-    return addrIPv6;                    /* return default in case of out-of-range index */
-  }
-
-#if !defined CMSIS_VIN
-// Add user code here:
-
-//   vioAddrIPv6[index] = ...;
-#endif
-
-  addrIPv6 = vioAddrIPv6[index];
-
-  return addrIPv6;
 }
--- Projects/Blinky/RTE/CMSIS/RTX_Config.h
+++ Projects/Blinky/RTE/CMSIS/RTX_Config.h
@@ -34,7 +34,7 @@
 #include  RTE_RTX_CONFIG_H
 #endif
 #endif
-
+ 
 #ifndef RTE_Compiler_EventRecorder
 #define OS_EVR_INIT                 0
 #endif
@@ -69,6 +69,61 @@
 //     <i> Default: 5
 #ifndef OS_ROBIN_TIMEOUT
 #define OS_ROBIN_TIMEOUT            5
+#endif
+ 
+//   </e>
+ 
+//   <e>Safety features (Source variant only)
+//   <i> Enables FuSa related features.
+//   <i> Requires RTX Source variant.
+//   <i> Enables:
+//   <i>  - selected features from this group
+//   <i>  - Thread functions: osThreadProtectPrivileged
+#ifndef OS_SAFETY_FEATURES
+#define OS_SAFETY_FEATURES          0
+#endif
+ 
+//     <q>Safety Class
+//     <i> Threads assigned to lower classes cannot modify higher class threads.
+//     <i> Enables:
+//     <i>  - Object attributes: osSafetyClass
+//     <i>  - Kernel functions: osKernelProtect, osKernelDestroyClass
+//     <i>  - Thread functions: osThreadGetClass, osThreadSuspendClass, osThreadResumeClass
+#ifndef OS_SAFETY_CLASS
+#define OS_SAFETY_CLASS             1
+#endif
+ 
+//     <q>MPU Protected Zone
+//     <i> Access protection via MPU (Spatial isolation).
+//     <i> Enables:
+//     <i>  - Thread attributes: osThreadZone
+//     <i>  - Thread functions: osThreadGetZone, osThreadTerminateZone
+//     <i>  - Zone Management: osZoneSetup_Callback
+#ifndef OS_EXECUTION_ZONE
+#define OS_EXECUTION_ZONE           1
+#endif
+ 
+//     <q>Thread Watchdog
+//     <i> Watchdog alerts ensure timing for critical threads (Temporal isolation).
+//     <i> Enables:
+//     <i>  - Thread functions: osThreadFeedWatchdog
+//     <i>  - Handler functions: osWatchdogAlarm_Handler
+#ifndef OS_THREAD_WATCHDOG
+#define OS_THREAD_WATCHDOG          1
+#endif
+ 
+//     <q>Object Pointer checking
+//     <i> Check object pointer alignment and memory region.
+#ifndef OS_OBJ_PTR_CHECK
+#define OS_OBJ_PTR_CHECK            0
+#endif
+ 
+//     <q>SVC Function Pointer checking
+//     <i> Check SVC function pointer alignment and memory region.
+//     <i> User needs to define a linker execution region RTX_SVC_VENEERS
+//     <i> containing input sections: rtx_*.o (.text.os.svc.veneer.*)
+#ifndef OS_SVC_PTR_CHECK
+#define OS_SVC_PTR_CHECK            0
 #endif
  
 //   </e>
@@ -146,6 +201,20 @@
 #define OS_IDLE_THREAD_TZ_MOD_ID    0
 #endif
  
+//   <o>Idle Thread Safety Class <0-15>
+//   <i> Defines the Safety Class number.
+//   <i> Default: 0
+#ifndef OS_IDLE_THREAD_CLASS
+#define OS_IDLE_THREAD_CLASS        0
+#endif
+ 
+//   <o>Idle Thread Zone <0-127>
+//   <i> Defines Thread Zone.
+//   <i> Default: 0
+#ifndef OS_IDLE_THREAD_ZONE
+#define OS_IDLE_THREAD_ZONE         0
+#endif
+ 
 //   <q>Stack overrun checking
 //   <i> Enables stack overrun check at thread switch (requires RTX source variant).
 //   <i> Enabling this option increases slightly the execution time of a thread switch.
@@ -213,6 +282,20 @@
 //   <i> Default: 0 (not used)
 #ifndef OS_TIMER_THREAD_TZ_MOD_ID
 #define OS_TIMER_THREAD_TZ_MOD_ID   0
+#endif
+ 
+//   <o>Timer Thread Safety Class <0-15>
+//   <i> Defines the Safety Class number.
+//   <i> Default: 0
+#ifndef OS_TIMER_THREAD_CLASS
+#define OS_TIMER_THREAD_CLASS       0
+#endif
+ 
+//   <o>Timer Thread Zone <0-127>
+//   <i> Defines Thread Zone.
+//   <i> Default: 0
+#ifndef OS_TIMER_THREAD_ZONE
+#define OS_TIMER_THREAD_ZONE        0
 #endif
  
 //   <o>Timer Callback Queue entries <0-256>
